6db5c5c9c3eb7a5454f91bcbb7909816104fd1f8
KALASH <kalashsonker22@gmail.com>
2022-06-03 13:23:44 +0530

    added code in suspend function

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -8,9 +8,28 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+val TAG="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //a suspend function will be called inside of a another suspend function or coroutine
+        GlobalScope  .launch {
+            val networkCallAnswer=doNetworkCall()
+            val networkCallAnswer2=doNetworkCall2()
+            Log.d(TAG,networkCallAnswer)
+            Log.d(TAG,networkCallAnswer2)
+        }
+    }
+    //we can create our own suspend functions outside onCreate function
+    //we cannot call a suspend function outside of a coroutine we can call
+    //it inside of a coroutine lineNo.
+    suspend fun  doNetworkCall(): String {
+        delay(3000L)
+        return "This is the answer"
+    }
+    suspend fun  doNetworkCall2(): String {
+        delay(3000L)
+        return "This is not the answer"
     }
 }
+
