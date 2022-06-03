d8d1524c8e3bfbc40183e84d957985f2299ad62a
KALASH <kalashsonker22@gmail.com>
2022-06-03 14:56:27 +0530

    added code in runBlocking

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -6,11 +6,21 @@ import android.util.Log
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+    val TAG="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //runBlocking will start a new coroutine in main thread and will block the main thread
 
+        Log.d(TAG,"Before runBlocking")
+        runBlocking {
+            Log.d(TAG,"Start of runBlocking")
+            delay(5000L)
+            Log.d(TAG,"End of runBlocking")
+        }
+        Log.d(TAG,"After runBlocking")
     }
 }
