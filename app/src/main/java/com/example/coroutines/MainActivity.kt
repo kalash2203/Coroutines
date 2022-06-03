fa334f2bb330e3c9db130e1f5556afe02ff537eb
KALASH <kalashsonker22@gmail.com>
2022-06-03 14:39:13 +0530

    added code in context

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -3,14 +3,29 @@ package com.example.coroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Context will describe in which thread
+        // coroutine will be started in
+GlobalScope.launch(Dispatchers.IO) {
+    Log.d(TAG, "Starting coroutines in thread${Thread.currentThread().name}")
+    val answer = doNetworkCall()
+    withContext(Dispatchers.Main) {
+        Log.d(TAG, "Setting text in thread ${Thread.currentThread().name}")
+        tvDummy.text = answer
+    }
+    }
+    }
+    suspend fun doNetworkCall(): String {
+        delay(3000L)
+        return "This is the answer"
     }
 }
