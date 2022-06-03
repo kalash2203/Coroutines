485720602a31adb1ee08c3dea021fa5fa066c7cb
KALASH <kalashsonker22@gmail.com>
2022-06-03 16:49:26 +0530

    added code in async

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -3,14 +3,32 @@ package com.example.coroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
+//async and await
 class MainActivity : AppCompatActivity() {
+    val TAG ="MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        GlobalScope.launch(Dispatchers.IO) {
+            val time = measureTimeMillis {
+           val answer1 =async { networkCall1()}
+                val answer2 =async { networkCall2()}
+            Log.d(TAG,"Answer1 is ${answer1.await()}")
+            Log.d(TAG,"Answer2 is ${answer2.await()}")
+        }
+            Log.d(TAG,"Requests took $time ms.")
+    }}
+    suspend fun networkCall1():String {
+        delay(3000L)
+        return "Answer 1"
+    }
+    suspend fun networkCall2():String {
+        delay(3000L)
+        return "Answer 2"
     }
 }
