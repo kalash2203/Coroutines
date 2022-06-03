e6e1addcaabb5680877205b02e503172a18809ec
KALASH <kalashsonker22@gmail.com>
2022-06-03 12:53:13 +0530

    delay() in coroutine

---
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //coroutine lives as long as the application lives
+        GlobalScope.launch {
+            //delay function only pause the current coroutine ,it will not block the
+            //whole thread.
+            //if main thread finishes then coroutine is also cancelled.
+            delay(3000L)
+            Log.d(TAG,"Coroutines says hello from thread${Thread.currentThread().name}")
+        }
+        Log.d(TAG," hello from thread${Thread.currentThread().name}")
+    }
+}
