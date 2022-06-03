18326a04883f3502bc1df9fb5ab015925776d973
KALASH <kalashsonker22@gmail.com>
2022-06-03 15:14:39 +0530

    added code in jobs

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -3,14 +3,30 @@ package com.example.coroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG="MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val job= GlobalScope.launch(Dispatchers.Default) {
+            repeat(5) {
+                Log.d(TAG,"Coroutines is still working...")
+                delay(1000L)
+            }
+        }
+        runBlocking {
+            delay(2000L)
+           //job.join will block the thread untill this(lineNo.15) coroutine is finished
+           // job.join()
+            job.cancel()
+            //used to cancel the job for that we have delayed the main thread lineNo.22
+            //cancellation is cooperative which means corutine should be properly setup
+            // in order to cancel it.
+            Log.d(TAG,"Main thread is continuing")
+        }
     }
 }
